a79d86bca4fd5ab5e291c53a33300416fa61a6dd
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2025-08-15 07:49:36 +0300

    Handle settings.js

--- LapShop.MVC/wwwroot/Modules/Settings.js
+++ LapShop.MVC/wwwroot/Modules/Settings.js
@@ -2,14 +2,24 @@
     GetAll: function () {
         Helper.AjaxCallGet("https://localhost:7128/api/Setting", {}, "json",
             function (data) {
-                console.log(data)
+
+                $('.logo').attr("src", `/Uploads/Settings/${data.logo}`);
+
+                $('#website-titel').text(data.websiteName);
+
+
                 $('#lnkFacebook').attr("href", data.facebookLink);
                 $('#lnkGoogle').attr("href", data.googleLink);
                 $('#lnkTwitter').attr("href", data.twitterLink);
                 $('#lnkInstagram').attr("href", data.instgramLink);
                 $('#lnkYoutube').attr("href", data.youtubeLink);
 
-                $('#website-titel').text(data.websiteName);
+                $('#middleBanner').attr("src", `/Uploads/Settings/${data.middleBanner}`);
+                $('#div-middleBanner').css("background-image", `url(/Uploads/Settings/${data.middleBanner})`);
+
+                $('#lastBanner').attr("src", `/Uploads/Settings/${data.lastBanner}`);
+
+
             },
             function (err) {
                 console.error("Failed to load settings", err);
